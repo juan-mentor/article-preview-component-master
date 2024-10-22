b6f4db73dc1a903f79cd8bec14a0ddf7cf39d5ff
juan reyes <juolre2@gmail.com>
2024-10-22 20:16:56 +0200

    estilos desktop

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const heroHiddenElement = document.querySelector('.hero__hidden');
 //  access all the links
 links.forEach(link => {
     link.addEventListener('click', () => {
+        
         // add or remove  active styles
         heroElement.classList.toggle('active');
         heroHiddenElement.classList.toggle('active');
