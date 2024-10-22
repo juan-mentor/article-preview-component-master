aadf43efe6633f930c1a5b2d7e9c03e5c513ab64
juan reyes <juolre2@gmail.com>
2024-10-22 13:52:45 +0200

    estilos mobile final

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//  elements capture
+const links = document.querySelectorAll('.hero__link');
+const heroElement = document.querySelector('.hero');
+const heroHiddenElement = document.querySelector('.hero__hidden');
+
+//  access all the links
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        // add or remove  active styles
+        heroElement.classList.toggle('active');
+        heroHiddenElement.classList.toggle('active');
+    })
+})
