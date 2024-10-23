a9958a8566f6a7173ea86255989e535fea16d212
juan reyes <juolre2@gmail.com>
2024-10-23 17:25:23 +0200

    estilos desktop

--- js/main.js
+++ js/main.js
@@ -3,12 +3,26 @@ const links = document.querySelectorAll('.hero__link');
 const heroElement = document.querySelector('.hero');
 const heroHiddenElement = document.querySelector('.hero__hidden');
 
+//  remove styles based on the browser's screen width
+window.addEventListener('resize', () => {
+    if ((window.innerWidth) > 787) {
+        heroHiddenElement.classList.remove('active');
+        heroElement.classList.remove('active');
+    } else {
+        heroHiddenElement.classList.remove('show');
+    }
+});
+
 //  access all the links
 links.forEach(link => {
     link.addEventListener('click', () => {
-        
-        // add or remove  active styles
-        heroElement.classList.toggle('active');
-        heroHiddenElement.classList.toggle('active');
-    })
-})
+        if ((window.innerWidth) > 787) {
+            // add or remove show styles
+            heroHiddenElement.classList.toggle('show');
+        } else {
+            // add or remove  active styles
+            heroElement.classList.toggle('active');
+            heroHiddenElement.classList.toggle('active');
+        }
+    });
+});
